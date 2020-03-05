437ff733e4a9c117d9dc99eafc787d379db17521
George Hart <georgehart@Georges-MacBook-Pro.local>
2020-03-05 12:04:23 -0800

    Initial Commit

---
+++ src/components/styled/Wrappers.js
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+export const FlexWrapper = styled.div`
+    display: flex;
+    ${props => props.position && `position: ${props.position};`}
+    flex-direction: ${props => props.flexDirection || "row"};
+    justify-content: ${props => props.justifyContent || "center"};
+    align-items: ${props => props.alignItems || "center"};
+    ${props => props.top && `top: ${props.top};`}
+    ${props => props.bottom && `bottom: ${props.bottom};`}
+    ${props => props.left && `left: ${props.left};`}
+    ${props => props.right && `right: ${props.right};`}
+    box-sizing: border-box;
+    padding: ${props => props.padding || "0"};
+    margin: ${props => props.margin || "0"};
+    height: ${props => props.height || "inherit"};
+    width: ${props => props.width || "inherit"};
+    background-color: ${props => props.background || "white"};
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,16 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import Layout from "../components/layout"
+
+import { Logo } from "../components/logo"
+import { FlexWrapper } from "../components/styled"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <FlexWrapper height="100vh" background="#ffe427" className="main-bg">
+      <SEO title="Home" />
+      <Logo className="logo__component" />
+    </FlexWrapper>
   </Layout>
 )
 
