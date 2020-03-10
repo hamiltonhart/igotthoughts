1753969ac5c842c94d982e4987d1776bb9f5de1b
George Hart <hamiltonhart@protonmail.com>
2020-03-09 20:36:06 -0700

    Wobble added to logo bounce.

--- src/components/logo/AnimatedLogo.js
+++ src/components/logo/AnimatedLogo.js
@@ -1,11 +1,12 @@
 import React from "react"
 import IGTLogo from "../../images/IGotThoughtsLogo.png"
-import { animated, useSpring } from "react-spring"
+import { animated, useSpring, config } from "react-spring"
 
 export const AnimatedLogo = () => {
   const spring = useSpring({
     from: { transform: "scale(0, 0)" },
     transform: "scale(1, 1)",
+    config: config.wobbly,
   })
   return (
     <animated.img
